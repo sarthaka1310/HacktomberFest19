cede53cfee355b160e42e5fa776aa9b39118a6fb
Ajini Perera <49752003+ajini-sahaejana@users.noreply.github.com>
2019-10-23 11:22:20 +0530

    A very simple hello world program using C Language

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Simple hello world program
+// Build with C Language
+// gcc -o hello_world hello_world.c
+// and run with
+// ./hello_world
+int main(void)
+{
+    printf("Hello World!");
+    return 0;
+}
